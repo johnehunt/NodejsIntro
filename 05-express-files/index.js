65108f430d1ec4b58d4541250f1ca0ac53d9e8d7
John Hunt <john@matraining.com>
2019-12-16 12:45:54 +0000

    Express example of sending files

---
+++ 05-express-files/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+
+// avoid hard coding the path
+const path = require('path')
+
+app.listen(8080, () => {
+  console.log("Server Running - http://localhost:8080");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'))
+});
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'about.html'))
+});
+
+app.get("/help", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'about.html'))
+});
+
+app.get("/contact", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'contact.html'))
+});
+
+// Handle unknown URL
+app.use(function(req, res, next) {
+  res.status(404).send("page not found");
+});

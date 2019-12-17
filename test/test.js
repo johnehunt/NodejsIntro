425331614567758dbe7ce12f57498b5549615b45
John Hunt <john@matraining.com>
2019-12-17 13:42:20 +0000

    default test location

---
+++ test/test.js
@@ -0,0 +1,33 @@
+// Default test location for Mocha
+// Can override this in package.json
+// e.g. "test": "mocha 08-testing/test/**/*.js"
+
+var assert = require('assert');
+var should = require('chai').should();
+
+describe('Object Test', function(){
+  it('should have property name', function(){
+    var car = {name:'Figo', Maker:'Ford'}
+
+    car.should.have.property('name');
+  });
+
+  it('should have property name with value Figo', function(){
+    var car = {name:'Figo', Maker:'Ford'}
+    car.should.have.property('name').equal('Figo');
+  });
+
+  it('should compare objects', function(){
+    var car = {name:'Figo', Maker:'Ford'}
+    var car1 = {name:'Figo', Maker:'Ford'}
+
+    // car.should.equal(car1);
+    car.should.deep.equal(car1);
+  });
+
+  it('handling null', function(){
+    var car = null;
+    //car.should.not.exist;
+    should.not.exist(car);
+  });
+});

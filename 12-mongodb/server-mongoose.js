7bf1adb41c62269b30bd53cb45a7737249a440d8
John Hunt <john@matraining.com>
2019-12-19 15:33:03 +0000

    General tidying

--- 12-mongodb/server-mongoose.js
+++ 12-mongodb/server-mongoose.js
@@ -6,21 +6,23 @@ const url = "mongodb://127.0.0.1/userdb";
 mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true });
 
 //Get the default connection
-const db = mongoose.connection;
+const connection = mongoose.connection;
 
 //Bind connection to error event (to get notification of connection errors)
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+connection.on(
+  "error",
+  console.error.bind(console, "MongoDB connection error:")
+);
 
 //Define a schema
-var Schema = mongoose.Schema;
-
-var UserModelSchema = new Schema({
+const Schema = mongoose.Schema;
+const UserModelSchema = new Schema({
   id: Number,
   name: String
 });
 
 // Compile model from schema
-var UserModel = mongoose.model("UserModel", UserModelSchema);
+const UserModel = mongoose.model("UserModel", UserModelSchema);
 
 var newUser = new UserModel({ id: 999, name: "James Lawson" });
 newUser.save(err => {
@@ -29,10 +31,7 @@ newUser.save(err => {
 });
 
 // Also create and save in one go
-UserModel.create({ id: 234, name: "Jos Jones" }, function(
-  err,
-  user
-) {
+UserModel.create({ id: 234, name: "Jos Jones" }, function(err, user) {
   if (err) return handleError(err);
   else console.log("New User Saved: " + user);
 });
@@ -48,20 +47,19 @@ UserModel.find({ name: "Jos Jones" }, "id name", (err, users) => {
 });
 
 // Can do it step by step
-var query = UserModel.find({})
+UserModel.find({})
   .select("name")
   .limit(5)
-  .sort({ name: -1 });
-
-query.exec((err, users) => {
-  if (err) return handleError(err);
-  else console.log(users);
-});
+  .sort({ name: -1 })
+  .exec((err, users) => {
+    if (err) return handleError(err);
+    else console.log(users);
+  });
 
 // Can remove a user
-UserModel.deleteOne({ id: 999 }, (err) => {
+UserModel.deleteOne({ id: 999 }, err => {
   if (err) return handleError(err);
-  else console.log('User deleted')
+  else console.log("User deleted");
 });
 
 // Can replace

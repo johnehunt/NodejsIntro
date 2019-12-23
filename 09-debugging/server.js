089c7c5807c994148d897364a8cd66932e44ee73
John Hunt <john@matraining.com>
2019-12-23 10:00:58 +0000

    Added express server to use with debugging

---
+++ 09-debugging/server.js
@@ -0,0 +1,44 @@
+// Express server to use with inspector for debugging
+const express = require("express");
+const app = express();
+
+console.debug("Starting Server");
+
+app.listen(8080, () => {
+  console.log("Server Running - http://localhost:8080");
+});
+
+app.get("/", (req, res) => {
+  console.debug("in default get/");
+  debugger; // Inserts a break point here
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Hello Express World!</h1><p>Welcome Everyone.</p>");
+  res.end();
+});
+
+app.get("/about", (req, res) => {
+  console.debug("in get /about");
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>About</h1><p>This is the about page.</p>");
+  res.end();
+});
+
+app.get("/help", (req, res) => {
+  console.debug("in get /help");
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Help</h1><p>This is the Help page.</p>");
+  res.end();
+});
+
+app.get("/contact", (req, res) => {
+  console.debug("in get /contact");
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Contacts</h1><p>This is the Contact page.</p>");
+  res.end();
+});
+
+// Handle unknown URL
+app.use(function(req, res, next) {
+  console.err("handling unknown URL: ", req.url);
+  res.status(404).send("page not found");
+});

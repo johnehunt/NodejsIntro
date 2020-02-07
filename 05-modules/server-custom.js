1da8e6456cc9aafdcf2be1a6e717b44be52ce371
John Hunt <jeh@midmarsh.co.uk>
2020-02-07 14:44:33 +0000

    Changed name of custom module

--- 05-modules/server-custom.js
+++ 05-modules/server-custom.js
@@ -11,7 +11,7 @@ const http = require("http");
 // avoid hard coding the path
 const path = require('path')
 // Load custom module
-const util = require(path.resolve(__dirname, "custom/utils"));
+const my_util = require(path.resolve(__dirname, "custom/my_utils"));
 
 //----------------------
 // Set up server
@@ -20,15 +20,15 @@ const util = require(path.resolve(__dirname, "custom/utils"));
 const server = http.createServer(function(req, res) {
   console.log("Handling", req.url);
   if (req.url == "/date")
-    res.write("<h1>Date</h1><p>" + util.getDate() + "</p>");
+    res.write("<h1>Date</h1><p>" + my_util.getDate() + "</p>");
   else if (req.url == "/time")
-    res.write("<h1>Time</h1><p>" + util.getTime() + "</p>");
+    res.write("<h1>Time</h1><p>" + my_util.getTime() + "</p>");
   else if (req.url == "/timestamp")
-    res.write("<h1>Timestamp</h1><p>" + util.getTimestamp() + "</p>");
+    res.write("<h1>Timestamp</h1><p>" + my_util.getTimestamp() + "</p>");
   else if (req.url == "/mod")
-    res.write("<h1>MOD</h1><p>" + util.getMOD() + "</p>");
+    res.write("<h1>MOD</h1><p>" + my_util.getMOD() + "</p>");
   else if (req.url == "/")
-    res.write("<h1>Hello World!</h1><p>Welcome Everyone - the MAX is " + util.MAX + ".</p>");
+    res.write("<h1>Hello World!</h1><p>Welcome Everyone - the MAX is " + my_util.MAX + ".</p>");
   else {
     res.writeHead(404);
     res.write("page not found");

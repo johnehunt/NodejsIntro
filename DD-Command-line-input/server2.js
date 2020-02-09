aa1b3058745d1fbac80f369f5cff6fc8d33fb45a
John Hunt <jeh@midmarsh.co.uk>
2020-02-09 12:49:22 +0000

    Examples illustrating handling command line inputs

---
+++ DD-Command-line-input/server2.js
@@ -0,0 +1,14 @@
+// Provides more options than basic readline
+const inquirer = require("inquirer");
+
+var questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What's your name?"
+  }
+];
+
+inquirer.prompt(questions).then(answers => {
+  console.log(`Hi ${answers["name"]}!`);
+});

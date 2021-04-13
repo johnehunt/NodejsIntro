de8243ef65b06b1e710b3683e31a5b09e43b221c
John Hunt <john.hunt10@gmail.com>
2021-04-13 14:03:01 +0100

    changed comments

--- 12-errors/errors1.js
+++ 12-errors/errors1.js
@@ -1,6 +1,6 @@
-// Comment out line 3 to run remaining lines
+// Example sof generating error types
 
-console.log(new Error('A standard error'))
+console.log(new Error('A standard error'));
 
 class UserError extends Error {
     constructor(args){
@@ -9,4 +9,4 @@ class UserError extends Error {
     }
 }
 
-console.log(new UserError('A standard error'))
+console.log(new UserError('A standard error'));

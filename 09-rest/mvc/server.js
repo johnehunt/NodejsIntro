64d3db6e36ce67ba739c234f6d6b27946ebb1955
John Hunt <john.hunt10@gmail.com>
2021-04-13 12:10:45 +0100

    Updated to express 4.17 style

--- 09-rest/mvc/server.js
+++ 09-rest/mvc/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-// To allow body to be parsed
-const bodyParser = require("body-parser");
+// To allow body to be parsed pre Express 4.16
+// const bodyParser = require("body-parser");
 
 // avoid hard coding the path
 const path = require("path");
@@ -10,17 +10,25 @@ const path = require("path");
 const config = require(path.resolve(__dirname, "config/default"));
 
 // Load user route definitions
-const users = require(path.resolve(__dirname, "controllers/users"));
+const controller = require(path.resolve(__dirname, "controllers/users"));
 
-// configure app to use bodyParser()
+// configure app to use bodyParser() pre 4.16
 // this will let us get the data from a POST
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true }));
+
+// Configure express to handle json body content
+// express 4.16 and newer
+app.use(express.json())
+app.use(express.urlencoded({
+  extended: true
+}))
 
 // Set up routing for our services
 const router = express.Router();
 
-// Log all messages sent to the server
+// Add logging middleware
+// to log all messages sent to the server
 router.use(function(req, res, next) {
   console.log("Received req.url: " + req.url);
   next(); // make sure we go to the next routes and don't stop here
@@ -33,14 +41,14 @@ router.get("/", function(req, res) {
 
 router
   .route("/users")
-  .get(users.getUsers)
-  .post(users.postUser)
-  .put(users.updateUser);
+  .get(controller.getUsers)
+  .post(controller.postUser)
+  .put(controller.updateUser);
 
 router
   .route("/users/:id")
-  .get(users.getUser)
-  .delete(users.deleteUser);
+  .get(controller.getUser)
+  .delete(controller.deleteUser);
 
 // All routes will be prefixed with /api
 app.use("/api", router);

5f36d169a6eae9dc52ebb1e8d26c8413f2a49ee4
John Hunt <john.hunt10@gmail.com>
2021-04-13 12:27:22 +0100

    Updates for Mocha

--- 10-testing/basic/test.js
+++ 10-testing/basic/test.js
@@ -4,10 +4,10 @@ const assert = require("assert");
 // Define a test specification
 describe("Basic Mocha String Test", function() {
   it("should return number of characters in a string", function() {
-    assert.equal("Hello".length, 5);
+    assert.strictEqual("Hello".length, 5);
   });
   it("should return first character of the string", function() {
-    assert.equal("Hello".charAt(0), "H");
+    assert.strictEqual("Hello".charAt(0), "H");
   });
   it.skip('should return -1 unless present', function() {
     // this test will not be run
--- 10-testing/nested/test.js
+++ 10-testing/nested/test.js
@@ -16,10 +16,10 @@ describe("depth1", function() {
     });
 
     it("should return number of characters in a string", function() {
-      assert.equal("Hello".length, 5);
+      assert.strictEqual("Hello".length, 5);
     });
     it("should return first character of the string", function() {
-      assert.equal("Hello".charAt(0), "H");
+      assert.strictEqual("Hello".charAt(0), "H");
     });
 
     afterEach(function() {

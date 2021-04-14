5ec6c041a4e7c7f048ed35202ce9d9cb5bfeaf40
John Hunt <john.hunt10@gmail.com>
2021-04-14 10:20:11 +0100

    Added further promise based examples

--- 06-promises/promise3.js
+++ 06-promises/promise3.js
@@ -1,43 +1,31 @@
-// Requires us to npm install request
-console.log("Starting HTTP Server");
+// Illustrates chanbing a sequence of then functions
 
-const http = require("http");
-// const request = require("request"); no longer supported
-const fetch = require("node-fetch");
+const promise = new Promise(function(resolve, reject) {
+    let x = 32;
+    let y = 0;
+    if (y != 0) {
+      resolve(x / y);
+    } else {
+      reject("Division by Zero");
+    }
+  });
 
-const server = http.createServer(function (req, resp) {
-  console.log("Handling", req.url);
-  resp.writeHead(200, { "Content-Type": "text/html" });
-  if (req.url == "/") {
-    resp.write("<h1>Hello World!</h1><p>Welcome Everyone.</p>");
-    resp.end();
-  } else if (req.url == "/user") {
-    // Setting URL and headers for request from guthub
-    var options = {
-      headers: {
-        "User-Agent": "request",
-      },
-    };
-
-    // Use node-fetch to call service as a promise
-    fetch("https://api.github.com/users/johnehunt", options)
-      .then((res) => res.json())
-      .then((userDetails) => {
-        console.log(userDetails);
-        resp.write(
-          "User details, user: " + userDetails.login + ", id: " + userDetails.id
-        );
-        resp.end();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-});
-
-console.log("Listening on port 8080");
-server.listen(8080);
-
-console.log("Started");
-console.log("For basic usage see - http://localhost:8080/");
-console.log("For user info see - http://localhost:8080/user");
+  promise
+  .then((result) => {
+    console.log("Did not divide by Zero");
+    return result
+  })
+  .then((result) => {
+    console.log(`Result is ${result}`);
+    return result
+  })
+  .then((value) => {
+      console.log('Finished processing result')
+  })
+  .catch((err) => {
+      console.log('first error')
+      throw err
+  })
+  .catch(function(errorMessage) {
+    console.log(errorMessage);
+  });
---
+++ 06-promises/server-promise.js
@@ -0,0 +1,43 @@
+// Requires us to npm install request
+console.log("Starting HTTP Server");
+
+const http = require("http");
+// const request = require("request"); no longer supported
+const fetch = require("node-fetch");
+
+const server = http.createServer(function (req, resp) {
+  console.log("Handling", req.url);
+  resp.writeHead(200, { "Content-Type": "text/html" });
+  if (req.url == "/") {
+    resp.write("<h1>Hello World!</h1><p>Welcome Everyone.</p>");
+    resp.end();
+  } else if (req.url == "/user") {
+    // Setting URL and headers for request from guthub
+    var options = {
+      headers: {
+        "User-Agent": "request",
+      },
+    };
+
+    // Use node-fetch to call service as a promise
+    fetch("https://api.github.com/users/johnehunt", options)
+      .then((res) => res.json())
+      .then((userDetails) => {
+        console.log(userDetails);
+        resp.write(
+          "User details, user: " + userDetails.login + ", id: " + userDetails.id
+        );
+        resp.end();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+});
+
+console.log("Listening on port 8080");
+server.listen(8080);
+
+console.log("Started");
+console.log("For basic usage see - http://localhost:8080/");
+console.log("For user info see - http://localhost:8080/user");

f76eae5ebf42fbfc00bf4f8498eb381fde8fe6ee
John Hunt <john.hunt10@gmail.com>
2021-04-14 11:47:19 +0100

    Updated to p[ost express 4.16 style

--- 15-mongoose/server/server.js
+++ 15-mongoose/server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-// To allow body to be parsed
-const bodyParser = require("body-parser");
+// To allow body to be parsed in Express pre 4.16
+// const bodyParser = require("body-parser");
 
 // avoid hard coding the path
 const path = require("path");
@@ -12,10 +12,19 @@ const config = require(path.resolve(__dirname, "config/default"));
 // Load user route definitions
 const users = require(path.resolve(__dirname, "controllers/users"));
 
-// configure app to use bodyParser()
+// configure app to use bodyParser() pre 4.16
 // this will let us get the data from a POST
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true }));
+
+// Configure express to handle json body content
+// express 4.16 and newer
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 // Set up routing for our services
 const router = express.Router();
@@ -55,7 +64,7 @@ function logErrors(err, req, res, next) {
 app.use(logErrors);
 
 // Can have more then one error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something went wrong!");
 });

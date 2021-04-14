1519c6f9ddbabb8d7b6bbd36aa6f4b6a2e82b415
John Hunt <john.hunt10@gmail.com>
2021-04-14 11:01:07 +0100

    Changed style of string

--- 02-intro/server.js
+++ 02-intro/server.js
@@ -2,16 +2,16 @@ console.log("Starting HTTP Server");
 // Load required http module - note naming convention
 // const holding reference to module is named after the
 // module itself
-const http = require("http");
+const http = require('http');
 
 // Create the server instance
 const server = http.createServer(function(req, res) {
-  res.write("Hello World!"); //write a response to the client
+  res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
 });
 
 // Set the port that the server will listen on
-console.log("Listening on port 8080");
+console.log('Listening on port 8080');
 server.listen(8080);
 
-console.log("Started - use http://localhost:8080/");
+console.log('Started - use http://localhost:8080/');

e30de8e4ffaef079d37b1f03b4ff293818a5e1a9
John Hunt <john.hunt10@gmail.com>
2021-04-14 11:06:45 +0100

    layout changes

--- 15-mongoose/server/controllers/users.js
+++ 15-mongoose/server/controllers/users.js
@@ -9,45 +9,58 @@ const Users = require("../models/users");
 
 function getUsers(req, res) {
   console.log("controller users getUsers()");
-  Users.getAllUsers().then((users) => {
-    res.json(users);
-  }).catch((error) => {
-    console.log(error);
-    res.status(500).send();
-  });
+  Users
+    .getAllUsers()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send();
+    });
 }
 
 function postUser(req, res) {
   console.log("postUser", req.body);
   const user = req.body;
-  Users.addUser(user).then((result) => {
-    res.status(201).send();
-  }).catch((error) => {
-    console.log(error);
-    res.status(500).send();
-  });
+  Users
+    .addUser(user)
+    .then((result) => {
+      res.status(201).send();
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send();
+    });
 }
 
 function updateUser(req, res) {
   console.log("updateUser", req.body);
   const user = req.body;
-  Users.updateUser(user).then((result) => {
-    res.status(201).send();
-  }).catch((error) => {
-    console.log(error);
-    res.status(500).send();
-  });
+  Users
+    .updateUser(user)
+    .then((result) => {
+      res.status(201).send();
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send();
+    });
 }
 
 function getUser(req, res) {
-  Users.getUserById(req.params.id).then((user) => {
-    res.json(user);    
-  }).catch((error) => {
-    console.log(error);
-    res.status(500).send();
-  });
+  Users
+    .getUserById(req.params.id)
+    .then((user) => {
+      res.json(user);    
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send();
+    });
 }
 
+// Alternative style using await illustrated in this function
 async function deleteUser(req, res) {
   const id = req.params.id;
   await Users.deleteUser(id);

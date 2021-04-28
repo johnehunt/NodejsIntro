1e47e6a244225be896a4f478e11d876c19a805f9
John Hunt <john.hunt10@gmail.com>
2021-04-28 16:09:38 +0100

    Added new session and example

---
+++ 6-npm-modules/server.js
@@ -0,0 +1,16 @@
+// Illustrates how the commander module
+// can be used to manage command line arguments
+const commander = require('commander');
+
+commander
+  .option('-d, --debug', 'output extra debugging')
+  .option('-l, --long', 'provide long form information')
+  .option('-p, --printer <message>', 'pretty print a message', 'hello');
+
+commander.parse(process.argv);
+
+const options = commander.opts();
+
+if (options.debug) console.log('debug mode turned on');
+if (options.long) console.log('This provides long form information');
+if (options.printer) console.log(`printer ${options.printer}`);

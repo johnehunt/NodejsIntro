ae2dd2b0815b85deefa95163daaf2ba1cb1794e1
John Hunt <john.hunt10@gmail.com>
2021-04-29 10:28:12 +0100

    Updated to use path to resolve path to file

--- 04-core/events/server1.js
+++ 04-core/events/server1.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
+const path = require("path");
 
-const rs = fs.createReadStream('./demofile.txt');
+const filename = path.resolve(__dirname, 'demofile.txt')
+const rs = fs.createReadStream(filename);
+
 rs.on('open', function () {
   console.log('The file is open');
 });

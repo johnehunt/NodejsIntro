0274d1821a31a7b6f0e9643f25ded0382bbc9253
John Hunt <john.hunt10@gmail.com>
2021-05-14 15:27:20 +0100

    Added exampe of setting port

--- 03-further-node/server3.js
+++ 03-further-node/server3.js
@@ -3,5 +3,9 @@ console.log("Command line arguments");
 console.log(process.argv);
 
 // Select the inputs to the program
-var myArgs = process.argv.slice(2);
+const myArgs = process.argv.slice(2);
 console.log("myArgs: ", myArgs);
+
+// if first argument is port to run off
+const port = parseInt(myArgs[0]);
+console.log('Start server on port:', port);

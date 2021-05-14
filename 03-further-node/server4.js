e307213a955fb0098fd751561121db4d4f360b6c
John Hunt <john.hunt10@gmail.com>
2021-05-14 15:37:36 +0100

    Refactoring of dotenv example

--- 03-further-node/server4.js
+++ 03-further-node/server4.js
@@ -1,16 +1,7 @@
-// Example illustrating environment variables
+// Illustrates accessing environment variables example
+console.log('Starting');
 
-// Set up the dotenv object
-// will look for files with ./env extension
-const dotenv = require("dotenv");
-const env_success = dotenv.config();
-
-if (env_success.error) {
-  throw env_success.error;
-}
-console.log(env_success.parsed);
-
-// Can now access data via process.env object
-console.log("Environment variable");
+console.log("Port environment variable");
 console.log(process.env.PORT);
-console.log(process.env.TITLE);
+
+console.log('Done');
